1fdeedddf033bef4d6c00009c37848d31663a988
kingways1 <otukaemeka10@gmail.com>
2026-06-26 08:43:12 -0700

    update

---
+++ js/hamburger.js
@@ -0,0 +1,8 @@
+const hamburger = document.getElementById("hamburger");
+const ahead = document.getElementById("ahead");
+
+hamburger.addEventListener("click", () => {
+    ahead.classList.toggle("show");
+}
+
+)
